b701598196466bba8b6c5b5d89ff1ca0638dc2fd
Samurai <franca.samurai@gmail.com>
2023-06-06 13:46:35 -0300

    fix: amount split

--- node/API/suppliers.ts
+++ node/API/suppliers.ts
@@ -2,16 +2,16 @@
 export async function provideSuppliersUsingMiniCart(ctx: Context) {
   const { response, state: { payload, affiliate } } = ctx
 
-  const commissionAmount = (payload.operationValue * affiliate.commissionPercentage) / 100
+  const amount = (payload.operationValue * affiliate.commissionPercentage) / 100
 
   const body = {
     id: affiliate.refId,
     name: affiliate.name,
-    amount: payload.operationValue,
+    amount: amount,
     document: affiliate.document,
     documentType: affiliate.documentType,
     role: 'influencer',
-    commissionAmount: commissionAmount,
+    commissionAmount: affiliate.commissionPercentage,
     chargebackLiable: false,
     chargeProcesssingFee: false,
   }
